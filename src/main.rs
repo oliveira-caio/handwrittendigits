242c83f758432f927810c97372e33004b387ecdd
caio <oliveiracaio1@yandex.com>
2021-03-03 08:55:57 -0300

    bla

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,28 @@ struct Network {
 
 impl Network {
     fn new(sizes: Vec<u64>) -> Network {
-	let num_layers = sizes.len() as u32;
-	let sizes = sizes;
-	let mut biases = Vec::new();
-	let mut weights = Vec::new();
-	let x = &sizes[..sizes.len()-1];
-	let y = &sizes[1..];
-	
-	for i in y.iter() {
-	    let layer = (0..*i).map(|_| rand::thread_rng().gen_range(-1.0,1.0)).collect();
-	    biases.push(layer);
-	}
-	
-	for i in 0..y.len() {
-	    let mut linha = Vec::new();
-	    for _ in 0..y[i] {
-		let coluna = (0..x[i]).map(|_| rand::thread_rng().gen_range(-1.0,1.0)).collect();
-		linha.push(coluna);
-	    }
-	    weights.push(linha);
-	}
-	
-	Network { num_layers, sizes, biases, weights }
+		let num_layers = sizes.len() as u32;
+		let sizes = sizes;
+		let mut biases = Vec::new();
+		let mut weights = Vec::new();
+		let x = &sizes[..sizes.len()-1];
+		let y = &sizes[1..];
+		
+		for i in y.iter() {
+			let layer = (0..*i).map(|_| rand::thread_rng().gen_range(-1.0,1.0)).collect();
+			biases.push(layer);
+		}
+		
+		for i in 0..y.len() {
+			let mut linha = Vec::new();
+			for _ in 0..y[i] {
+				let coluna = (0..x[i]).map(|_| rand::thread_rng().gen_range(-1.0,1.0)).collect();
+				linha.push(coluna);
+			}
+			weights.push(linha);
+		}
+		
+		Network { num_layers, sizes, biases, weights }
     }
 }
 
