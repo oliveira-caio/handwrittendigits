811fa02918e5bb31116e677f4e88045a6b1518b6
caio <oliveiracaio1@yandex.com>
2021-04-07 08:38:37 -0300

    bla

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use byteorder::ReadBytesExt;
 
 #[derive(Debug)]
 struct Network {
-	num_layers: u8,
+	num_layers: u16,
 	sizes: Vec<u16>,
 	biases: Vec<Vec<f32>>,
 	weights: Vec<Vec<Vec<f32>>>,
@@ -23,6 +23,7 @@ struct MnistData {
 	data: Vec<u8>,
 }
 
+#[derive(Debug)]
 pub struct MnistImage {
 	pub image: Array2<f64>,
 	pub classification: u8,
@@ -35,17 +36,17 @@ pub fn load_data(dataset_name: &str) -> Result<Vec<MnistImage>, std::io::Error> 
 	let images_data = &MnistData::new(&(File::open(filename))?)?;
 	let mut images: Vec<Array2<f64>> = Vec::new();
 	let image_shape = (images_data.sizes[1] * images_data.sizes[2]) as usize;
-
+	
 	for i in 0..images_data.sizes[0] as usize {
 		let start = i * image_shape;
-		let image_data = images_data.data[start..start + image_shape].to_vec();
+		let image_data = images_data.data[start..(start + image_shape)].to_vec();
 		let image_data: Vec<f64> = image_data.into_iter().map(|x| x as f64 / 255.).collect();
-		images.push(Array2::from_shape_vec((image_shape, 1), image_data).unwrap());
+		images.push(Array2::from_shape_vec((image_shape, 1), image_data).unwrap());		
 	}
 
 	let classifications: Vec<u8> = label_data.data.clone();
 	let mut ret: Vec<MnistImage> = Vec::new();
-
+	
 	for (image, classification) in images.into_iter().zip(classifications.into_iter()) {
 		ret.push(MnistImage {
 			image,
@@ -88,7 +89,7 @@ impl MnistData {
 
 impl Network {
 	fn new(sizes: Vec<u16>) -> Network {
-		let num_layers = sizes.len() as u8;
+		let num_layers = sizes.len() as u16;
 		let sizes = sizes;
 		let mut biases = Vec::new();
 		let mut weights = Vec::new();
@@ -123,18 +124,18 @@ impl Network {
 		   test_data:Option<&Vec<(Vec<f32>,Vec<f32>)>>) {
 		let mut i: usize = 0;
 		let mut mini_batches: Vec<Vec<(Vec<f32>, Vec<f32>)>> = Vec::new();
-		let mut training_data_shuffled = Vec::new();
 
 		for j in 0..epochs {
-			training_data_shuffled = my_shuffle(&training_data);
-			
 			while i < training_data.len() {
-				mini_batches.push(training_data_shuffled[i..i+mini_batch_size].to_vec());
+				mini_batches.push(
+					my_shuffle(&training_data)[i..i+mini_batch_size].to_vec()
+				);
 				i += mini_batch_size;
 			}
 
 			for mini_batch in mini_batches.iter() {
 				Network::update_mini_batch(self, mini_batch, eta);
+				println!("{:?}, \n {:?} \n", training_data[j].0, training_data[j].1);
 			}
 
 			match test_data {
@@ -219,14 +220,14 @@ impl Network {
 			let mut z = activation.clone();
 			
 			for (b,w) in self.biases.iter().zip(self.weights.iter()) {
- 				z = dot(w, &z)
+				z = dot(w, &z)
 					.iter()
 					.zip(b.iter())
 					.map(|(&u, &v)| u+v)
 					.collect();
 				zs.push(z.clone());
 				activations.push(vec_sigmoid(&z));
-                z = vec_sigmoid(&z);
+				z = vec_sigmoid(&z);
 			}
 			
 			let mut delta: Vec<f32> = Network::cost_derivative(&activations[activations.len() - 1], &y)
@@ -261,13 +262,7 @@ impl Network {
 	fn cost_derivative(output_activations: &Vec<f32>, y: &Vec<f32>) -> Vec<f32> {
 		output_activations.iter().zip(y.iter()).map(|(&u, &v)| u-v).collect()
 	}
-}
-
-
-fn matriz_de_zeros(linhas: usize, colunas: usize) -> Vec<Vec<f32>> {
-	vec![vec![0.0; colunas]; linhas]
-}
-	
+}	
 
 fn multiplica_matrizes(matriz1: &Vec<Vec<f32>>, matriz2: &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
 	let mut aux = 0.0;
@@ -293,14 +288,12 @@ fn soma_vetores(vetor1: &Vec<f32>, vetor2: &Vec<f32>) -> Vec<f32> {
 fn soma_matrizes(matriz1: &Vec<Vec<f32>>, matriz2: &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
 	let mut matriz3: Vec<Vec<f32>> = vec![vec![0.0; matriz1[0].len()]; matriz1.len()];
 	
-	//println!("{:?}\n\n {:?}\n\n {:?}\n\n", matriz1, matriz2, matriz3);
 	for i in 0..matriz1.len() {
 		for j in 0..matriz1[0].len() {
 			matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
 		}
 	}
 	
-
 	matriz3
 }
 
@@ -371,8 +364,9 @@ fn main() {
     let net_sizes: Vec<u16> = vec![784,30,10];
     let mut net = Network::new(net_sizes);
 
-    let train = load_data("train").unwrap();
+    let train = load_data("t10k").unwrap();
     let mut training_data = Vec::new();
+	
     for i in 0..train.len() {
         let input_iter = train[i].image.into_iter();
         let mut input: Vec<f32> = Vec::new();
@@ -384,18 +378,8 @@ fn main() {
         training_data.push((input, output))
     }
 
-
-    /*
-    let mut training_data = Vec::new();
-    for _ in 0..10000 {
-        let input: Vec<f32> = (0..2).map(|_| rand::thread_rng().gen_range(-1.0..1.0)).collect();
-        let output: Vec<f32> = (0..1).map(|_| 0.0).collect();
-        training_data.push((input, output))
-    }
-    */
-
-    println!("Starting...");
-    net.sgd(&training_data, 10, 100, 3.0, Some(&training_data));
+	println!("Started.");
+    net.sgd(&training_data, 30, 10, 3.0, Some(&training_data));
     let acc = net.evaluate(training_data);
     println!("{:?}", acc);
 }
