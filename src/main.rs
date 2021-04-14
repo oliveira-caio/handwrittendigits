ab603e96a4e21dd1f13998f29c654bacacab6562
caio <oliveiracaio1@yandex.com>
2021-04-14 10:45:42 -0300

    converted everything to ndarray, except for the main, and resolved a few errors, but there are eight errors left

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use rand::seq::SliceRandom;
-use rand::Rng;
 use std::f32::consts::E;
-use ndarray::Array2;
+use ndarray::{array, Array, Array1, Array2};
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Uniform;
 use std::io::{Cursor, Read};
 use flate2;
 use flate2::read::GzDecoder;
 use std::fs::File;
 use byteorder::BigEndian;
 use byteorder::ReadBytesExt;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 #[derive(Debug)]
 struct Network {
 	num_layers: u16,
 	sizes: Vec<u16>,
-	biases: Vec<Vec<f32>>,
-	weights: Vec<Vec<Vec<f32>>>,
+	biases: Vec<Array1<f32>>,
+	weights: Vec<Array2<f32>>,
 }
 
 #[derive(Debug)]
@@ -30,7 +31,8 @@ pub struct MnistImage {
 	pub classification: u8,
 }
 
-pub fn load_data(dataset_name: &str) -> Result<Vec<MnistImage>, std::io::Error> {
+pub fn load_data(dataset_name: &str)
+				 -> Result<Vec<MnistImage>, std::io::Error> {
 	let filename = format!("data/{}-labels-idx1-ubyte.gz", dataset_name);
 	let label_data = &MnistData::new(&(File::open(filename))?)?;
 	let filename = format!("data/{}-images-idx3-ubyte.gz", dataset_name);
@@ -96,34 +98,32 @@ impl Network {
 		let mut weights = Vec::new();
 
 		for y in sizes[1..].iter() {
-			biases.push(vetor_aleatorio(*y as usize));
+			biases.push(aleatory_vec(*y as usize));
 		}
 
 		for (y,x) in sizes[1..].iter().zip(sizes[..sizes.len() - 1].iter()) {
-			weights.push(matriz_aleatoria(*y as usize, *x as usize));
+			weights.push(aleatory_matrix(*y as usize, *x as usize));
 		}
 
 		Network { num_layers, sizes, biases, weights }
     }
 
-	fn feedforward(&self, vetor: Vec<f32>) -> Vec<f32> {
-		let mut result = vetor;
+	fn feedforward(&self, vector: Array1<f32>) -> Array1<f32> {
+		let mut result = vector;
 
 		for (b, w) in self.biases.iter().zip(self.weights.iter()) {
-			result = vec_sigmoid(&dot(w, &result)
-								 .iter()
-								 .zip(b.iter())
-								 .map(|(&u, &v)| u+v)
-								 .collect());
+			result = vec_sigmoid(&(w.dot(&result) + b));
 		}
+		
 		result
 	}
 
-	fn sgd(&mut self, training_data: &Vec<(Vec<f32>, Vec<f32>)>, epochs: usize,
+	fn sgd(&mut self, training_data: &Vec<(Array1<f32>, Array1<f32>)>,
+		   epochs: usize,
 		   mini_batch_size: usize, eta: f32,
-		   test_data:Option<&Vec<(Vec<f32>,Vec<f32>)>>) {
+		   test_data:Option<&Vec<(Array1<f32>,Array1<f32>)>>) {
 		let mut i: usize = 0;
-		let mut mini_batches: Vec<Vec<(Vec<f32>, Vec<f32>)>> = Vec::new();
+		let mut mini_batches: Vec<Vec<(Array1<f32>, Array1<f32>)>> = Vec::new();
 
 		for j in 0..epochs {
             println!("Shuffling");
@@ -153,107 +153,87 @@ impl Network {
 		}
 	}
 
-	fn evaluate(&self, test_data: Vec<(Vec<f32>, Vec<f32>)>) -> u16 {
+	fn evaluate(&self, test_data: Vec<(Array1<f32>, Array1<f32>)>) -> u16 {
 		let mut test_results = Vec::new();
-		let mut soma = 0;
+		let mut sum = 0;
 
-		for i in 0..test_data.len() {
-            let a = &test_data[i].0;
-            let b = &test_data[i].1;
-			let x = Network::feedforward(&self, a.to_vec());
-			test_results.push((x,b));
+		for (u, v) in test_data {
+			let x = Network::feedforward(&self, u);
+			test_results.push((x,v));
 		}
 
 		for (u,v) in test_results.iter() {
        		if argmax(&u) == argmax(&v) {
-				soma += 1;
+				sum += 1;
 			}
 		}
 
-		soma
+		sum
 	}
 
-	fn update_mini_batch(&mut self, mini_batch: &Vec<(Vec<f32>,Vec<f32>)>, eta: f32) {
+	fn update_mini_batch(&mut self,
+						 mini_batch: &Vec<(Array1<f32>,Array1<f32>)>,
+						 eta: f32) {
 		let mut nabla_b = Vec::new();
 		let mut nabla_w = Vec::new();
-
-		for i in 0..self.biases.len() {
-			nabla_b.push(vec![0.0; (self.biases[i]).len()]);
+		
+		for b in self.biases {
+			nabla_b.push(Array::zeros(b.shape()));
 		}
 
-		for i in 0..self.weights.len() {
-			nabla_w.push(vec![vec![0.0; self.weights[i][0].len()];
-							  self.weights[i].len()]);
+		for w in self.weights {
+			nabla_w.push(Array::zeros(w.shape()));
 		}
 
 		for (x,y) in mini_batch.iter() {
 			let (delta_nabla_b, delta_nabla_w) = Network::backprop(self, x, y);
 			
 			for i in 0..delta_nabla_b.len() {
-				nabla_b[i] = soma_vetores(&nabla_b[i], &delta_nabla_b[i]);
+				nabla_b[i] = nabla_b[i] + delta_nabla_b[i];
 			}
 			
 			for i in 0..nabla_w.len() {
-				nabla_w[i] = soma_matrizes(&nabla_w[i], &delta_nabla_w[i]);
+				nabla_w[i] = nabla_w[i] + delta_nabla_w[i];
 			}
 		}
 
-
 		for i in 0..self.biases.len() {
-			for j in 0..self.biases[i].len() {
-				self.biases[i][j] = self.biases[i][j] - nabla_b[i][j] * eta / (mini_batch.len() as f32);
-			}
+			self.biases[i] = self.biases[i] - nabla_b[i] * eta / (mini_batch.len() as f32);
 		}
 
 		for i in 0..self.weights.len() {
-			for j in 0..self.weights[i].len() {
-				for k in 0..self.weights[i][j].len() {
-					self.weights[i][j][k] = self.weights[i][j][k] - nabla_w[i][j][k] * eta / (mini_batch.len() as f32);
-				}
-			}
+			self.weights[i] = self.weights[i] - nabla_w[i] * eta / (mini_batch.len() as f32);
 		}
 	}
 	
-	fn backprop(&self, activation: &Vec<f32>, y: &Vec<f32>) ->
-		(Vec<Vec<f32>>, Vec<Vec<Vec<f32>>>) {
-			let mut nabla_b: Vec<Vec<f32>> = Vec::new();
-			let mut nabla_w: Vec<Vec<Vec<f32>>> = Vec::new();
+	fn backprop(&self, activation: &Array1<f32>, output: &Array1<f32>) ->
+		(Vec<Array1<f32>>, Vec<Array2<f32>>) {
+			let mut nabla_b: Vec<Array1<f32>> = Vec::new();
+			let mut nabla_w: Vec<Array2<f32>> = Vec::new();
 			let mut activations = vec![activation.clone()];
 			let mut zs = Vec::new();
 			let mut z = activation.clone();
 			
 			for (b,w) in self.biases.iter().zip(self.weights.iter()) {
-				z = dot(w, &z)
-					.iter()
-					.zip(b.iter())
-					.map(|(&u, &v)| u+v)
-					.collect();
+				z = w.dot(&z) + b;
 				zs.push(z.clone());
 				activations.push(vec_sigmoid(&z));
 				z = vec_sigmoid(&z);
 			}
 			
-			let mut delta: Vec<f32> = Network::cost_derivative(&activations[activations.len() - 1], &y)
-				.iter()
-				.zip(sigmoid_prime(&zs[zs.len() - 1]).iter())
-				.map(|(&u,&v)| u*v)
-				.collect();
+			let mut delta: Array1<f32> = Network::cost_derivative(
+				&activations[activations.len() - 1],
+				&output
+			) * sigmoid_prime(&zs[zs.len() - 1]);
 			nabla_b.push(delta.clone());
-			nabla_w.push(multiplica_matrizes(&transpose(&vec![delta.clone()]),
-										&vec![activations[activations.len() - 2].clone()]));
+			nabla_w.push(delta.t().dot(&activations[activations.len() - 2]));
 			
 			for l in 2..self.num_layers {
 				z = zs[zs.len() - l as usize].clone();
 				let sp = sigmoid_prime(&z);
-				delta = dot(&transpose(&self.weights[self.weights.len() - (l as usize) + 1]),
-							&delta)
-					.iter()
-					.zip(sp.iter())
-					.map(|(&u,&v)| u*v)
-					.collect();
+				delta = self.weights[self.weights.len() - (l as usize) + 1].dot(&delta) * sp;
 				nabla_b.push(delta.clone());
-				nabla_w.push(multiplica_matrizes(&transpose(&vec![delta.clone()]),
-												 &vec![activations[activations.len() - (l as usize) - 1].clone()]));
+				nabla_w.push(delta.t().dot(&activations[activations.len() - (l as usize) - 1]));
 			}
 
 			nabla_b.reverse();
@@ -262,29 +242,12 @@ impl Network {
 			(nabla_b, nabla_w)
 		}
 	
-	fn cost_derivative(output_activations: &Vec<f32>, y: &Vec<f32>) -> Vec<f32> {
-		output_activations.iter().zip(y.iter()).map(|(&u, &v)| u-v).collect()
+	fn cost_derivative(output_activations: &Array1<f32>, output: &Array1<f32>) -> Array1<f32> {
+		output_activations.iter().zip(output.iter()).map(|(&u, &v)| u-v).collect()
 	}
 }	
 
-fn multiplica_matrizes(matriz1: &Vec<Vec<f32>>, matriz2: &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
-	let mut aux = 0.0;
-	let mut matriz3 = vec![vec![0.0; matriz2[0].len()]; matriz1.len()];
-
-	for i in 0..matriz1.len() {
-		for j in 0..matriz2[0].len() {
-			for k in 0..matriz2.len() {
-				aux += matriz1[i][k] * matriz2[k][j];
-			}
-			matriz3[i][j] = aux;
-			aux = 0.0;
-		}
-	}
-
-	matriz3
-}
-
-fn argmax(v: &Vec<f32>) -> usize {
+fn argmax(v: &Array1<f32>) -> usize {
 	let x = v[0];
 	let mut index = 0;
 
@@ -293,73 +256,28 @@ fn argmax(v: &Vec<f32>) -> usize {
 			index = i;
 		}
 	}
+	
 	index
-}
-
-fn soma_vetores(vetor1: &Vec<f32>, vetor2: &Vec<f32>) -> Vec<f32> {
-	vetor1.iter().zip(vetor2.iter()).map(|(&u, &v)| u+v).collect()
-}
-
-fn soma_matrizes(matriz1: &Vec<Vec<f32>>, matriz2: &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
-	let mut matriz3: Vec<Vec<f32>> = vec![vec![0.0; matriz1[0].len()]; matriz1.len()];
-	
-	for i in 0..matriz1.len() {
-		for j in 0..matriz1[0].len() {
-			matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
-		}
-	}
-	
-	matriz3
-}
-
-fn transpose(matriz: &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
-	let mut transposta = vec![vec![0.0; matriz.len()]; matriz[0].len()];
-
-	for i in 0..matriz.len() {
-		for j in 0..matriz[i].len() {
-			transposta[j][i] = matriz[i][j];
-		}
-	}
-
-	transposta
-}
-
-fn dot(matriz: &Vec<Vec<f32>>, vetor: &Vec<f32>) -> Vec<f32> {
-	let mut resultado = vec![0.0; matriz.len()];
-	
-	for i in 0..matriz.len() {
-		for j in 0..vetor.len() {
-			resultado[i] += matriz[i][j] * vetor[j];
-		}
-	}
-
-	resultado
 }
 
 fn sigmoid(x: &f32) -> f32 {
 	1.0 / (1.0 + f32::powf(E,-x))
 }
 
-fn vec_sigmoid(vetor: &Vec<f32>) -> Vec<f32> {
-	vetor.iter().map(|&x| sigmoid(&x)).collect()
+fn vec_sigmoid(vetor: &Array1<f32>) -> Array1<f32> {
+	vetor.map(|&x| sigmoid(&x))
 }
 
-fn sigmoid_prime(vetor: &Vec<f32>) -> Vec<f32> {
-	vetor.iter().map(|&x| sigmoid(&x)*(1.0-sigmoid(&x))).collect()
+fn sigmoid_prime(vetor: &Array1<f32>) -> Array1<f32> {
+	vetor.mapv(|x| sigmoid(&x)*(1.0-sigmoid(&x)))
 }
 
-fn vetor_aleatorio(tamanho: usize) -> Vec<f32> {
-	(0..tamanho).map(|_| rand::thread_rng().gen_range(-1.0..1.0)).collect()
+fn aleatory_vec(tamanho: usize) -> Array1<f32> {
+	Array::random(tamanho, Uniform::new(-1., 1.))
 }
 
-fn matriz_aleatoria(linhas: usize, colunas: usize) -> Vec<Vec<f32>> {
-	let mut matriz = vec![vec![0.0; colunas]; linhas];
-
-	for i in 0..linhas {
-		matriz[i] = vetor_aleatorio(colunas);
-	}
-
-	matriz
+fn aleatory_matrix(linhas: usize, colunas: usize) -> Array2<f32> {
+	Array::random((linhas, colunas), Uniform::new(-1., 1.))
 }
 
 fn my_shuffle<T: Clone>(vetor: &[T]) -> Vec<T> {
@@ -376,7 +294,7 @@ fn my_shuffle<T: Clone>(vetor: &[T]) -> Vec<T> {
 }
 
 fn main() {
-    let net_sizes: Vec<u16> = vec![784,30,10];
+    let net_sizes: Array1<u16> = array![784,30,10];
     let mut net = Network::new(net_sizes);
 
     let train = load_data("train").unwrap();
@@ -404,8 +322,6 @@ fn main() {
         output[test[i].classification as usize] = 1.0;
         test_data.push((input, output))
     }
-
-
 
 	println!("Started.");
     net.sgd(&training_data, 30, 100, 3.0, Some(&test_data));
